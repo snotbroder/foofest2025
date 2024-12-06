6271aaa54d0726ee0a427a26d0f9063f7105b199
Joris <jote0002@stud.kea.dk>
2024-12-06 13:28:52 +0100

    oprettet sandbox, forsøger at få det til at virke

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { getBands } from "../app/api";
-
+import "./globals.css";
 export default async function Home() {
   const bands = await getBands();
   console.log(bands);
@@ -8,23 +8,26 @@ export default async function Home() {
   return (
     <div className="">
       <div className="flex gap-5 ">
-        <Link className="text-blue-700 hover:text-red-600 text-xl" href="/app/sandbox/sofie">
+        <Link className="text-blue-700 hover:text-red-600 text-xl" href="/sandbox/sofie">
           Sofies sandbox
         </Link>
-        <Link className="text-blue-700 hover:text-red-600 text-xl" href="/app/sandbox/julie">
+        <Link className="text-blue-700 hover:text-red-600 text-xl" href="/sandbox/julie">
           Julies sandbox
         </Link>
-        <Link className="text-blue-700 hover:text-red-600 text-xl" href="/app/sandbox/joris">
+        <Link className="text-blue-700 hover:text-red-600 text-xl" href="/sandbox/joris">
           Joris' sandbox
         </Link>
       </div>
+      <h1 className="font-spicy text-main-1">TEXT HERE</h1>
+      <p className="text-main-1 font-rethink font-bold">This is a paragraph</p>
+      <p className="text-accent-1 font-rethink font-semibold">This is a paragraph semibold</p>
       <div className="grid grid-cols-6 text-gray-300">
         {bands.map((band, index) => {
           return (
-            <h1 key={band.slug}>
+            <p key={band.slug}>
               {band.name}
               <span>{index}</span>
-            </h1>
+            </p>
           );
         })}
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      fontFamily: {
+        spicy: ["Spicy Rice", "times new roman"],
+        rethink: ["Rethink Sans", "ui-sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "main-1": "#3A140B",
+        "main-2": "#FFF8DB",
+        primary: "#DBD976",
+        secondary: "#77B09F",
+        tertiary: "#D76F2D",
+        "accent-1": "#F0047F",
+        "accent-2": "#A20F81",
+        "feedback-error": "#A11B04",
+        "feedback-succeed": "#99B836",
+        "feedback-disabled-1": "#646464",
+        "feedback-disabled-2": "hsla(0 0% 39.2% / 0.2)",
+      },
+      textColor: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        "main-1": "#3A140B",
+        "main-2": "#FFF8DB",
+        primary: "#DBD976",
+        secondary: "#77B09F",
+        tertiary: "#D76F2D",
+        "accent-1": "#F0047F",
+        "accent-2": "#A20F81",
+        "feedback-error": "#A11B04",
+        "feedback-succeed": "#99B836",
+        "feedback-disabled-1": "#646464",
+        "feedback-disabled-2": "hsla(0 0% 39.2% / 0.2)",
+      },
+      borderRadius: {
+        "rounded-reg": "6px",
+      },
+      border: {
+        "border-thin": "2px",
+        "border-thick": "3px",
+      },
+      margin: {
+        desktop: "8rem",
+        mobile: "2rem",
       },
     },
   },
