1614afd5590e2e0ee4ede2e924ccd58045db34e1
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-12-06 23:41:43 +0100

    ændret størrelse af card

--- src/app/sandbox/sofie/components/BandCard.jsx
+++ src/app/sandbox/sofie/components/BandCard.jsx
@@ -2,12 +2,8 @@ import Image from "next/image";
 
 export default function BandCard({ imgSrc, altText, name }) {
   return (
-    <div className=" flex w-80 flex-col shadow-custom">
-      <img
-        className=" h-auto md:h-80 md:w-fit  object-cover "
-        src={imgSrc}
-        alt={altText}
-      />
+    <div className=" flex w-96 flex-col shadow-custom">
+      <img className=" h-80 object-cover " src={imgSrc} alt={altText} />
       <div className="bg-primary ">
         <h3 className="font-spicy uppercase text-main-1 text-center text-2xl md:text-3xl py-3">
           {name}
