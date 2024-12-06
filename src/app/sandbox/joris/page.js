07b02e1e96c59edf426ec7b32fba40bc384fb664
Joris <jote0002@stud.kea.dk>
2024-12-06 21:24:10 +0100

    Første forsøg, brug med state og ændrer farve/status baseret på statet fra page

--- src/app/sandbox/joris/page.js
+++ src/app/sandbox/joris/page.js
@@ -1,12 +1,39 @@
+"use client";
+import BookingSteps from "@/components/BookingSteps";
+import { useState } from "react";
 import "./main.css";
 import Link from "next/link";
 export default function Home() {
+  const [state, setState] = useState(0);
+
   return (
     <>
       <Link href="/" className="text-blue-700 hover:text-red-600 text-xl">
         Home
       </Link>
       <div className="hello">Joris' sandbox</div>
+      <BookingSteps state={state}></BookingSteps>
+
+      <p>
+        state is <span className="font-bold text-red-600">{state}</span>
+      </p>
+
+      <button
+        className="border-main-1 border-2"
+        onClick={() => {
+          setState((prevState) => prevState - 1);
+        }}
+      >
+        state -1
+      </button>
+      <button
+        className="border-main-1 border-2"
+        onClick={() => {
+          setState((prevState) => prevState + 1);
+        }}
+      >
+        state +1
+      </button>
     </>
   );
 }
