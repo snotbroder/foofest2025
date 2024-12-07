4f8f34dcb578e388eef029b1465307479c6eeb06
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-12-07 21:46:44 +0100

    tilføjet billede, tilføjet tekst, props og baggrunds color

---
+++ src/app/sandbox/sofie/ClickableCard1.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+function ClickableCard1({ header, text, src, alt }) {
+  return (
+    <div className="w-96  bg-tertiary p-4 text-main-1 font-rethink flex flex-col gap-4">
+      <Image
+        src={src}
+        width={500}
+        height={500}
+        alt={alt}
+        className="object-cover "
+      ></Image>
+      <div>
+        <h4 className="text-xl font-bold">{header}</h4>
+        <p className="text-sm md:text-base">{text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ClickableCard1;
--- src/app/sandbox/sofie/page.js
+++ src/app/sandbox/sofie/page.js
@@ -1,4 +1,5 @@
 import "./main.css";
+import ClickableCard1 from "./ClickableCard1";
 import Link from "next/link";
 export default function Home() {
   return (
@@ -7,6 +8,23 @@ export default function Home() {
         Home
       </Link>
       <div className="hello">Sofies sandbox</div>
+      <div className="flex gap-32 flex-wrap">
+        <ClickableCard1
+          src="/tshirt.webp"
+          header="Buy the t-shirt of this year’s festival"
+          text="Embrace the groove and wear the love! Get your official Foo Festival T-shirt and keep the peace, love, and music alive wherever you go"
+        ></ClickableCard1>
+        <ClickableCard1
+          src="/musicquiz.webp"
+          header="Join the quiz and win a free ticket"
+          text="Think you know your music trivia? Test your knowledge in our quiz for a chance to win a ticket to Foo Festival 2024! Don't miss out on the fun—peace, love, and music await"
+        ></ClickableCard1>
+        <ClickableCard1
+          src="/volunteerPoster.webp"
+          header="Become a volunteer"
+          text="As a volunteer, you’ll be part of the magic—setting up, guiding guests, and keeping the festival running smoothly. In return, you’ll get free entry, official merch, and a chance to meet amazing people while gaining valuable experience."
+        ></ClickableCard1>
+      </div>
     </>
   );
 }
