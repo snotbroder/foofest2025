f6cb8e751089bdb7a2d64fa851af84a017794f84
Joris <jote0002@stud.kea.dk>
2024-12-07 20:58:30 +0100

    Lavet amiuntBtn færdig, med state som bliver sendt og fanget som prop

--- src/app/sandbox/joris/page.js
+++ src/app/sandbox/joris/page.js
@@ -1,14 +1,22 @@
+"use client";
+import { useState } from "react";
 import AmountBtn from "./AmountBtn";
 import "./main.css";
 import Link from "next/link";
+
 export default function Home() {
+  const [amount, setAmount] = useState(0);
+  function handleAmountChange(newAmount) {
+    setAmount(newAmount);
+  }
   return (
     <>
       <Link href="/" className="text-blue-700 hover:text-red-600 text-xl">
         Home
       </Link>
       <div className="hello">Joris' sandbox</div>
-      <AmountBtn></AmountBtn>
+      <AmountBtn onAmountChange={handleAmountChange}></AmountBtn>
+      <span>amount is {amount}</span>
     </>
   );
 }
