bfa42cd491027090361e24ad9d42a27683c24f68
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-12-07 23:35:09 +0100

    ændret størrelse på overskrift

--- src/app/sandbox/sofie/ClickableCard1.jsx
+++ src/app/sandbox/sofie/ClickableCard1.jsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 function ClickableCard1({ header, text, src, alt }) {
   return (
-    <div className="w-96  bg-tertiary p-4 text-main-1 font-rethink flex flex-col gap-4 shadow-custom">
+    <div className="w-96 bg-tertiary p-4 text-main-1  font-rethink flex flex-col gap-1">
       <Image
         src={src}
-        width={500}
-        height={500}
+        width={400}
+        height={400}
         alt={alt}
         className="object-cover "
       ></Image>
       <div>
-        <h4 className="text-xl font-bold">{header}</h4>
+        <h4 className="text-2xl font-bold">{header}</h4>
         <p className="text-sm md:text-base">{text}</p>
       </div>
     </div>
