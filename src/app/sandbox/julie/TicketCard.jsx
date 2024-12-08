92bdcae88d2ef407f1d569604a53abd07de161d8
julievictoria98 <jura0002@stud.kea.dk>
2024-12-08 17:03:18 +0100

    tilføjet label

--- src/app/sandbox/julie/TicketCard.jsx
+++ src/app/sandbox/julie/TicketCard.jsx
@@ -1,13 +1,27 @@
-function TicketCard({ bgColor, title, ticketType, text, price, labelText }) {
+function TicketCard({
+  bgColor,
+  title,
+  ticketType,
+  text,
+  price,
+  labelText,
+  label,
+}) {
   return (
     <div>
       <div
         className={` bg-primary flex flex-col gap-4 pt-6 p-8  w-56 h-72 ${ticketType} `}
       >
-        <h2 className=" font-semibold text-main-1 text-3xl">{title}</h2>
-        <h3 className="text-main-1 text-2xl">{price}</h3>
+        <h2 className=" font-rethink font-semibold text-main-1 text-3xl">
+          {title}
+        </h2>
+        <h3 className=" font-rethink text-main-1 text-2xl">{price}</h3>
         <p>{text}</p>
-        <div>{labelText}</div>
+        <div
+          className={`bg-secondary font-rethink flex self-end  rounded-rounded-reg px-2 cardlabel cardlabel-container ${label}`}
+        >
+          {labelText}
+        </div>
       </div>
     </div>
   );
