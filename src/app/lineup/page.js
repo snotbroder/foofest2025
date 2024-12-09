21dabea2bcd367adc6c5da3307db86014183860a
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-12-09 19:06:13 +0100

    tilføjet lineup page, tilføjet overskrift og bandcards

--- src/app/lineup/page.js
+++ src/app/lineup/page.js
@@ -1,8 +1,8 @@
 import BandCards from "../components/BandCards";
-
 function Lineup() {
   return (
-    <div>
+    <div className="flex flex-col gap-16 pt-60">
+      <h1 className="font-spicy text-6xl">Lineup 2025</h1>
       <BandCards></BandCards>
     </div>
   );
