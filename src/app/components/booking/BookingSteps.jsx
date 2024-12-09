619ae3f998e5e16bb3cfe5b4d7d5b711eb71a48d
Joris <jote0002@stud.kea.dk>
2024-12-09 14:24:01 +0100

    slettet gammel kode

--- src/app/components/booking/BookingSteps.jsx
+++ src/app/components/booking/BookingSteps.jsx
@@ -17,32 +17,6 @@ function BookingSteps({ step, stepHandler }) {
           {stepName}
         </li>
       ))}
-      {/* <li
-        className={`${step === 0 ? "text-secondary font-bold" : step > 0 ? "underline text-main-1" : "text-feedback-disabled-1"} `}
-        onClick={() => {
-          step <= 0 ? console.log("step is 0 or less. step=", step) : console.log("step is not 0 or less. step=", step);
-        }}
-      >
-        Choose tickets
-      </li>
-      <li
-        className={`${step === 1 ? "text-secondary font-bold" : step > 1 ? "underline text-main-1" : "text-feedback-disabled-1"} `}
-        onClick={() => {
-          step <= 1 ? thisStep(1) : console.log("step is not 1 or less. step=", step);
-        }}
-      >
-        Choose camp
-      </li>
-      <li
-        className={`${step === 2 ? "text-secondary font-bold" : step > 2 ? "underline text-main-1" : "text-feedback-disabled-2"} `}
-        onClick={() => {
-          step <= 2 ? console.log("step is 2 or less. step=", step) : console.log("step is not 2 or less. step=", step);
-        }}
-      >
-        Contact info
-      </li>
-      <li className={`${step === 3 ? "text-secondary font-bold" : step > 3 ? "underline text-main-1" : "text-feedback-disabled-2"} `}>Payment info</li>
-      <li className={`${step === 4 ? "text-secondary font-bold" : step > 4 ? "underline text-main-1" : "text-feedback-disabled-2"} `}>Overview</li> */}
     </ul>
   );
 }
