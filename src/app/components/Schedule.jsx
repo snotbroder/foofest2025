b34dda02fa60a91d728bd8d0b630c6c31d2a0e20
julievictoria98 <jura0002@stud.kea.dk>
2024-12-10 10:07:59 +0100

    tester lige react-dom-lastte i ny branch og swr

--- src/app/components/Schedule.jsx
+++ src/app/components/Schedule.jsx
@@ -1,35 +1,56 @@
-import { getSchedule, getBands } from "../api";
-import ScheduleBox from "./ScheduleBox";
-import { IoMdArrowDropdown } from "react-icons/io";
-import "../globals.css";
+// import { getSchedule, getBands } from "../api";
+// import ScheduleBox from "./ScheduleBox";
+// import { IoMdArrowDropdown } from "react-icons/io";
+// import "../globals.css";
 
-async function Schedule() {
-  const scheduleData = await getSchedule();
-  const schedule = scheduleData.Jotunheim.mon;
+// async function Schedule() {
+//   const scheduleData = await getSchedule();
+//   const schedule = scheduleData.Jotunheim.mon;
 
-  //   let listOfScenes = [];
+//   //   let listOfScenes = [];
 
-  //   for (let scene in scheduleData) {
-  //     listOfScenes.push(scene);
-  //   }
+//   //   for (let scene in scheduleData) {
+//   //     listOfScenes.push(scene);
+//   //   }
 
-  //   console.log(listOfScenes);
-  //   console.log(bands);
-  //   const bands = await getBands();
-  //   console.log(bands);
+//   //   console.log(listOfScenes);
+//   //   console.log(bands);
+//   //   const bands = await getBands();
+//   //   console.log(bands);
+
+//   return (
+//     <article className=" -mx-mobile flex flex-col justify-center gap-4">
+//       {schedule.map((event) => {
+//         return (
+//           <ScheduleBox
+//             key={event.index}
+//             artist={event.act}
+//             startTime={event.start}
+//             endTime={event.end}
+//           />
+//         );
+//       })}
+//     </article>
+//   );
+// }
+
+// export default Schedule;
+
+function Schedule({ scheduleData }) {
+  if (!scheduleData || scheduleData.length === 0) {
+    return <div>No events found for this day and stage.</div>;
+  }
 
   return (
     <article className=" -mx-mobile flex flex-col justify-center gap-4">
-      {schedule.map((event) => {
-        return (
-          <ScheduleBox
-            key={event.index}
-            artist={event.act}
-            startTime={event.start}
-            endTime={event.end}
-          />
-        );
-      })}
+      {scheduleData.map((event, index) => (
+        <div key={index} className="schedule-box">
+          <h2>{event.act}</h2>
+          <p>
+            {event.start} - {event.end}
+          </p>
+        </div>
+      ))}
     </article>
   );
 }
