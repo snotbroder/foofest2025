d6e25e52b037342a357ddd5f735b1965a0f39188
julievictoria98 <jura0002@stud.kea.dk>
2024-12-12 23:32:12 +0100

    små styling ændringer

--- src/app/lineup/page.js
+++ src/app/lineup/page.js
@@ -12,11 +12,9 @@ function Lineup() {
       <div className="relative"></div>
       <div className="flex flex-col gap-16 pt-48">
         <h1 className="font-spicy text-6xl">Lineup 2025</h1>
-        <div className="bg-secondary -mx-mobile lg:-mx-desktop pt-16 mt-10 md:mt-40">
-          {/* <LineUpFilter pickedGenre={"jazz"} /> */}
-          <LineUpComponent />
-          {/* <BandCards></BandCards> */}
-        </div>
+        {/* <LineUpFilter pickedGenre={"jazz"} /> */}
+        <LineUpComponent />
+        {/* <BandCards></BandCards> */}
       </div>
     </div>
   );
