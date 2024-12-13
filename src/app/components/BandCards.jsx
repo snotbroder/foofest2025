aa14f42c6dbf7a376ab4367d163f35a231b872f8
Joris <jote0002@stud.kea.dk>
2024-12-13 10:16:01 +0100

    ændret alle localhost fetches til gabbydull

--- src/app/components/BandCards.jsx
+++ src/app/components/BandCards.jsx
@@ -2,8 +2,10 @@ import { getBands } from "../api";
 import BandCard from "./BandCard";
 
 async function BandCards() {
-  const BASE_URL = "http://localhost:8080/logos/";
-  const response = await fetch("http://localhost:8080/bands", {
+  const BASE_URL = "https://gabby-dull-drip.glitch.me/logos/";
+  const response = await fetch("https://gabby-dull-drip.glitch.me/bands", {
+    // const BASE_URL = "http://localhost:8080/logos/";
+    // const response = await fetch("http://localhost:8080/bands", {
     method: "GET",
   });
   const bands = await response.json();
@@ -11,19 +13,8 @@ async function BandCards() {
   return (
     <div className="flex flex-wrap justify-center md:justify-between gap-10 mx-mobile lg:mx-desktop">
       {bands.map((band) => {
-        const logo = band.logo.startsWith("http")
-          ? band.logo
-          : `${BASE_URL}${band.logo}`;
-        return (
-          <BandCard
-            alt={`Logo of ${band.name}`}
-            key={band.name}
-            imgSrc={logo}
-            name={band.name}
-            genre={band.genre}
-            slug={band.slug}
-          />
-        );
+        const logo = band.logo.startsWith("http") ? band.logo : `${BASE_URL}${band.logo}`;
+        return <BandCard alt={`Logo of ${band.name}`} key={band.name} imgSrc={logo} name={band.name} genre={band.genre} slug={band.slug} />;
       })}
     </div>
   );
