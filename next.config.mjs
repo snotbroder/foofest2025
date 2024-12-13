a64bec7d18dc2d3833406ece3a6d52c072279b31
Joris <jote0002@stud.kea.dk>
2024-12-13 10:33:15 +0100

    exportér cnfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,28 @@
+Sofie Kjær-Madsen
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "localhost",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
