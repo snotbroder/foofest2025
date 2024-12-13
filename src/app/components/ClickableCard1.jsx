13a36869ddd2340224d307aa9caa02d4caa5c394
Joris <jote0002@stud.kea.dk>
2024-12-13 11:33:46 +0100

    rekommenteret billeder på bandcard og clickablecard

--- src/app/components/ClickableCard1.jsx
+++ src/app/components/ClickableCard1.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 function ClickableCard1({ header, text, src, alt }) {
   return (
     <div className="w-72 shadow-custom h-auto bg-tertiary p-4 text-main-1  font-rethink flex flex-col gap-1">
-      <div className="relative w-full h-64">{/* <Image fill src={src} alt={alt} className="object-cover"></Image> */}</div>
+      <div className="relative w-full h-64">
+        <Image fill src={src} alt={alt} className="object-cover"></Image>
+      </div>
       <div>
         <h3 className="font-bold">{header}</h3>
         <p className="">{text}</p>
