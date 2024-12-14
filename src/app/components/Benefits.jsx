8a25fdf949024c6aa24cfd1dd152e922188abbc0
julievictoria98 <jura0002@stud.kea.dk>
2024-12-14 16:33:37 +0100

    det samme som tidligere push

--- src/app/components/Benefits.jsx
+++ src/app/components/Benefits.jsx
@@ -5,24 +5,24 @@ import { FaShower } from "react-icons/fa6";
 
 function Benefits() {
   return (
-    <section className="bg-secondary -mx-mobile px-4 py-20 text-main-2 flex   flex-col lg:-mx-desktop text-lg gap-20  font-rethink text-center justify-center ">
-      <h1 className="text-3xl md:text-4xl font-semibold">BENEFITS</h1>
-      <div className=" flex-col flex lg:flex-row gap-20 text-center justify-center items-center ">
+    <section className="bg-secondary -mx-mobile px-4 py-20 flex flex-col lg:-mx-desktop text-lg gap-20  font-rethink text-center justify-center ">
+      <h2 className=" text-main-2 font-semibold">BENEFITS</h2>
+      <div className=" flex-col flex lg:flex-row gap-20 text-center justify-center items-center text-main-2  ">
         <div className="flex flex-col gap-2 items-center max-w-36">
           <IoTicketOutline className="lg:size-11 size-9" />
-          <p>One free meal pr day</p>
+          <p className="text-main-2 ">One free meal pr day</p>
         </div>
         <div className="flex flex-col gap-2 items-center  max-w-36">
           <MdOutlineFastfood className="lg:size-11 size-9" />
-          <p>One free ticket for the festival</p>
+          <p className="text-main-2 ">One free ticket for the festival</p>
         </div>
         <div className="flex flex-col gap-2 items-center max-w-36">
           <RiDiscountPercentLine className="lg:size-11 size-9" />
-          <p>10% discount on your next ticket</p>
+          <p className="text-main-2 ">10% discount on your next ticket</p>
         </div>
         <div className="flex flex-col gap-2 items-center max-w-36">
           <FaShower className="lg:size-11 size-9" />
-          <p>Free access to the hot showers</p>
+          <p className="text-main-2 ">Free access to the hot showers</p>
         </div>
       </div>
     </section>
--- src/app/voluenteer/page.js
+++ src/app/voluenteer/page.js
@@ -1,32 +1,38 @@
 import Benefits from "../components/Benefits";
 import ClickableCard1 from "../components/ClickableCard1";
 import VolunteerForm from "../components/VoluenteerForm";
+import Image from "next/image";
 
 function voluenteer() {
   return (
-    <article className="flex flex-col gap-24 mt-20 ">
-      <section className="flex flex-col gap-4  text-main-1 ">
-        <h1 className=" text-4xl md:text-6xl font-semibold font-spicy">
-          BECOME A VOLUENTEER
-        </h1>
-        <h2 className=" md:text-2xl text-lg font-rethink">
+    <article className="flex relative flex-col -mx-desktop  overflow-hidden  gap-24 mt-20 ">
+      <div className=" absolute   -right-52 ">
+        <Image
+          src="/illustrations/star.svg"
+          width={600}
+          height={600}
+          alt="pink flower/star illustration"
+        />
+      </div>
+      <section className="flex flex-col gap-4  text-main-1">
+        <h1 className="font-semibold font-spicy">BECOME A VOLUENTEER</h1>
+        <h2 className=" font-rethink">
           Do you want to become a part of foofest team 2025?
         </h2>
-        <p className="font-rethink md:text-lg">
+        <p className="font-rethink ">
           Join the Team Behind "CampEase" in Camping Area Helheim and Bring
           Comfort to the Festival life.
         </p>
-        <button className="bg-accent-1 md:text-base text-sm shadow-lg shadow-black flex self-start px-4 py-0.5">
+        <button className="bg-accent-1  shadow-lg shadow-black flex self-start px-4 py-0.5">
           Apply now!
         </button>
       </section>
-      <section>
-        <Benefits />
-      </section>
+
+      <Benefits />
       <div className="flex flex-col gap-8">
-        <h3 className="text-3xl md:text-4xl font-semibold text-center text-main-1">
+        <h2 className="font-semibold text-center text-main-1">
           LEARN MORE ABOUT THE JOBS
-        </h3>
+        </h2>
         <div className="flex flex-wrap items-center justify-center xl:justify-between  gap-10 ">
           <ClickableCard1
             src="/img/tshirt.webp"
@@ -42,9 +48,9 @@ function voluenteer() {
         </div>
       </div>
       <div className="flex flex-col gap-8 lg:items-center">
-        <h4 className="text-3xl md:text-4xl font-semibold text-center text-main-1">
+        <h2 className="font-semibold text-center text-main-1">
           SEND A APPLICATION TODAY
-        </h4>
+        </h2>
         <VolunteerForm />
       </div>
     </article>
