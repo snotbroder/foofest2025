13b6be473d832c140eccc33ec481d27bb7d00c69
Joris <jote0002@stud.kea.dk>
2024-12-14 13:43:18 +0100

    fjernet "tickets" når man har "max selected"

--- src/app/components/AmountBtn.jsx
+++ src/app/components/AmountBtn.jsx
@@ -14,9 +14,6 @@ function AmountBtn({ onAmountChange, maxAmount }) {
       //send det opdaterede state gennem onAmountChange prop
       onAmountChange(newAmount);
     }
-
-    //Vises ikke akkurat
-    console.log("amount is: ", amount);
   }
 
   function subtract() {
@@ -25,12 +22,10 @@ function AmountBtn({ onAmountChange, maxAmount }) {
       setAmount(newAmount);
       onAmountChange(newAmount);
     }
-
-    console.log("amount is: ", amount);
   }
 
   return (
-    <div className={`flex flex-col relative ${amount === maxAmount ? "after:content-['Max_selected_tickets'] after:absolute after:-bottom-4 after:left-0 after:text-main-1 after:text-xs " : ""}`}>
+    <div className={`flex flex-col relative ${amount === maxAmount ? "after:content-['Max_selected'] after:absolute after:-bottom-4 after:left-0 after:text-main-1 after:text-xs " : ""}`}>
       <div className="flex place-items-center gap-1">
         <button onClick={subtract} className={`${amount === 0 ? "bg-feedback-disabled-2 cursor-not-allowed border-feedback-disabled-1 hover:border-feedback-disabled-1 active:bg-feedback-disabled-2" : "bg-main-2"} relative disabled:opacity-60 font-rethink aspect-square px-3 py-1 text-main-1 text-2xl border-solid border-main-1 border-2 after:bg-main-1 after:absolute after:top-0 after:left-0 after:-z-10 after:content-[''] after:w-full after:h-full after:ml-1 after:mt-1 hover:after:mt-0 hover:after:ml-0 after:transition-all after:duration-75 hover:outline-1 hover:border-accent-1 hover:text-main-1 hover:transition-all active:bg-accent-1`}>
           -
