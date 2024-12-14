b2c60043b4bd7d3858461d8dff61be023703d44b
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-12-14 20:33:21 +0100

    tilføjet illustration i footer

--- src/app/components/layout-components/Footer.jsx
+++ src/app/components/layout-components/Footer.jsx
@@ -5,11 +5,20 @@ import {
   FaRegCopyright,
 } from "react-icons/fa";
 import Image from "next/image";
+
 function Footer() {
   return (
-    <footer className="mt-20 md:-mx-desktop -mx-mobile overflow-hidden">
-      <span className="before:block before:h-px  before:bg-main-1  "></span>
-      <div className="flex flex-col gap-8 md:gap-11 my-11 md:my-16 mx-mobile md:mx-desktop">
+    <footer className="relative  mt-44 md:-mx-desktop -mx-mobile overflow-hidden before:absolute before:content-[''] before:w-full before:h-[0.5px] before:bg-main-1 ">
+      <div className="absolute -right-28 opacity-60 w-96 top-6 rotate-180">
+        <Image
+          src="illustrations/svg/flowerOrange.svg"
+          width={440}
+          height={440}
+          objectFit="cover"
+          alt="Orange Flower"
+        ></Image>
+      </div>
+      <div className="flex flex-col gap-12 md:gap-10 my-11 md:my-16 mx-mobile md:mx-desktop ">
         <div className="font-rethink flex gap-7 center flex-col md:flex-row md:gap-40 ">
           <ul>
             <li className="font-bold">About</li>
@@ -58,7 +67,6 @@ function Footer() {
         <FaRegCopyright />
         2024, Foo Festival
       </span>
-      <span className="block relative after:content-[''] after:bg-[url('/img/flower1.svg')] after:bg-cover after:w-80 after:h-80 lg:after:w-96 lg:after:h-96 after:opacity-60 after:absolute after:-bottom-10 after:-right-20 md:after:-right-0 md:after:-bottom-10 lg:after:-right-20 lg:after:-bottom-16 after:z-[-1] "></span>
     </footer>
   );
 }
