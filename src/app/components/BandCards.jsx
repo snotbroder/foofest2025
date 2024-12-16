0171b4faa1481b7a3ed3c5466b1f8f22e26c6ba9
Joris <jote0002@stud.kea.dk>
2024-12-16 13:52:28 +0100

    dfghj

--- src/app/components/BandCards.jsx
+++ src/app/components/BandCards.jsx
@@ -11,7 +11,9 @@ async function BandCards() {
   return (
     <div className="flex flex-wrap justify-center md:justify-between gap-10 mx-mobile lg:mx-desktop">
       {bands.map((band) => {
-        const logo = band.logo.startsWith("https://") ? band.logo : `${BASE_URL}/logos/${band.logo}`;
+        // const bandImage = bandImagePath.startsWith("https://picsum") ? matchingBand.logo
+        // : `${endpointBase}/logos/${matchingBand.logo}`;
+        const logo = band.logo.startsWith("https://picsum") ? band.logo : `${BASE_URL}/logos/${band.logo}`;
 
         return <BandCard alt={`Logo of ${band.name}`} key={band.name} imgSrc={logo} name={band.name} genre={band.genre} slug={band.slug} />;
       })}
