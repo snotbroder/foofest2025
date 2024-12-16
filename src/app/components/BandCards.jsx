29eb7ccab130cd968434d5eb8b29b553987ffe3c
Joris <jote0002@stud.kea.dk>
2024-12-16 13:47:03 +0100

    dngies

--- src/app/components/BandCards.jsx
+++ src/app/components/BandCards.jsx
@@ -2,7 +2,7 @@ import { getBands } from "../api";
 import BandCard from "./BandCard";
 
 async function BandCards() {
-  const BASE_URL = "https://gabby-dull-drip.glitch.me";
+  const BASE_URL = "https://gabby-dull-drip.glitch.me/";
   const response = await fetch("https://gabby-dull-drip.glitch.me/bands", {
     method: "GET",
   });
@@ -11,7 +11,7 @@ async function BandCards() {
   return (
     <div className="flex flex-wrap justify-center md:justify-between gap-10 mx-mobile lg:mx-desktop">
       {bands.map((band) => {
-        band.logo.startsWith("https://picsum") ? band.logo : `${BASE_URL}/logos/${band.logo}`;
+        band.logo.startsWith("https://") ? band.logo : `${BASE_URL}/logos/${band.logo}`;
 
         return <BandCard alt={`Logo of ${band.name}`} key={band.name} imgSrc={logo} name={band.name} genre={band.genre} slug={band.slug} />;
       })}
