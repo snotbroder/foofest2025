d71463331f2d9bac9959f80d053cafa3eabd09fe
Joris <jote0002@stud.kea.dk>
2024-12-17 11:22:49 +0100

    fået kna ptil at se disabled ud

--- src/app/components/booking/TicketSelectParent.jsx
+++ src/app/components/booking/TicketSelectParent.jsx
@@ -3,12 +3,11 @@
 import TicketSelectCard from "./TicketSelectCard";
 import { useBasketStore } from "@/stores/basket-stores";
 import { useBasketFunctionality } from "@/stores/basket-functionality";
+import { useState } from "react";
 
 function TicketSelectParent() {
   //Multiply funktion i store som sørger for at opdatere dataen korrekt/de rigtige steder i arrayet
-  const updateTicketMultiply = useBasketStore(
-    (state) => state.updateTicketMultiply
-  );
+  const updateTicketMultiply = useBasketStore((state) => state.updateTicketMultiply);
 
   const handleUpdate = (ticketType, newAmount) => {
     // opdater ticketAmount i basket store baseret på den ticketType følger med
@@ -19,29 +18,21 @@ function TicketSelectParent() {
   const totalTickets = useBasketStore((state) => state.totalTickets());
   const step = useBasketFunctionality((step) => step.bookingStep);
   const setNewStep = useBasketFunctionality((state) => state.setNewStep);
+
   return (
     <>
-      <TicketSelectCard
-        onAmountChange={(amount) => handleUpdate("vip ticket", amount)}
-        ticketName="VIP TICKET"
-        variant="type1"
-        price="1299"
-        subText="Best Offer"
-      ></TicketSelectCard>
+      <TicketSelectCard onAmountChange={(amount) => handleUpdate("vip ticket", amount)} ticketName="VIP TICKET" variant="type1" price="1299" subText="Best Offer"></TicketSelectCard>
 
-      <TicketSelectCard
-        onAmountChange={(amount) => handleUpdate("regular ticket", amount)}
-        ticketName="REGULAR TICKET"
-        variant="type2"
-        price="799"
-      ></TicketSelectCard>
+      <TicketSelectCard onAmountChange={(amount) => handleUpdate("regular ticket", amount)} ticketName="REGULAR TICKET" variant="type2" price="799"></TicketSelectCard>
       <button
         onClick={() => {
           if (totalTickets === 0) {
             console.log("no");
+
+            return;
           } else setNewStep(step + 1);
         }}
-        className="button grid place-self-center lg:place-self-end"
+        className={`button  grid place-self-center lg:place-self-end ${totalTickets === 0 ? "opacity-60 cursor-not-allowed" : ""}`}
       >
         Next step
       </button>
