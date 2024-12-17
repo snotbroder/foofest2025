c98c6774c1233ac7582daac5ba55ed4db36930d2
Joris <jote0002@stud.kea.dk>
2024-12-17 17:49:31 +0100

    fixet

--- src/app/components/booking/TicketSelectParent.jsx
+++ src/app/components/booking/TicketSelectParent.jsx
@@ -19,11 +19,21 @@ function TicketSelectParent() {
   const step = useBasketFunctionality((step) => step.bookingStep);
   const setNewStep = useBasketFunctionality((state) => state.setNewStep);
 
+  const vipTicket = useBasketStore((state) => {
+    const vipTicket = state.ticketInfo.find((item) => item.itemTitle === "vip ticket");
+    return vipTicket.itemMultiply;
+  });
+
+  const regularTicket = useBasketStore((state) => {
+    const regularTicket = state.ticketInfo.find((item) => item.itemTitle === "regular ticket");
+    return regularTicket.itemMultiply;
+  });
+
   return (
     <>
       <TicketSelectCard storedValue={vipTicket} onAmountChange={(amount) => handleUpdate("vip ticket", amount)} ticketName="VIP TICKET" variant="vip" variantImage="vip-image" price="1299" subText="Best Offer"></TicketSelectCard>
 
-      <TicketSelectCard storedValue={storedValue} onAmountChange={(amount) => handleUpdate("regular ticket", amount)} ticketName="REGULAR TICKET" variant="regular" variantImage="regular-image" price="799"></TicketSelectCard>
+      <TicketSelectCard storedValue={regularTicket} onAmountChange={(amount) => handleUpdate("regular ticket", amount)} ticketName="REGULAR TICKET" variant="regular" variantImage="regular-image" price="799"></TicketSelectCard>
       <button
         onClick={() => {
           if (totalTickets === 0) {
