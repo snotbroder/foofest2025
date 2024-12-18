f05cf45e795029a7df94421aa467348459091b54
julievictoria98 <jura0002@stud.kea.dk>
2024-12-18 11:25:22 +0100

    ændret hostname i next.config

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
 
       {
         protocol: "https",
-        hostname: "sudsy-jet-grill.glitch.me",
+        hostname: process.env.NEXT_PUBLIC_GLITCH_URL,
       },
     ],
   },
