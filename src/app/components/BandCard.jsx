5f6c4ed80ffcd9b43586af887c9fdd9e5007413a
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-12-18 13:21:57 +0100

    ordnet bandcards størrelser

--- src/app/components/BandCard.jsx
+++ src/app/components/BandCard.jsx
@@ -10,15 +10,15 @@ export default function BandCard({ genre, imgSrc, alt, name, slug }) {
           <div className="absolute  right-0 top-5 rounded-rounded-reg mr-1  bg-tertiary uppercase py-0.5 px-5 z-10  ">
             <p className="small  text-main-2">{genre}</p>
           </div>
-          <Image
-            className="fill"
-            src={imgSrc}
-            alt={alt}
-            fill
-            style={{
-              objectFit: "cover",
-            }}
-          />
+          <div className="h-full w-full absolute object-cover">
+            <Image
+              className="fill"
+              src={imgSrc}
+              alt={alt}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw"
+            />
+          </div>
         </div>
         <div className="bg-primary h-16 flex justify-center items-center w-full   ">
           <h3 className="uppercase  text-center  py-5">{name}</h3>
