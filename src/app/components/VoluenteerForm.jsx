7f8da2492c588ba3b199b6e3fbe336be373763c8
julievictoria98 <jura0002@stud.kea.dk>
2024-12-18 08:14:34 +0100

    rettet apply now knap på volunteer-side

--- src/app/components/VoluenteerForm.jsx
+++ src/app/components/VoluenteerForm.jsx
@@ -1,7 +1,6 @@
 import { postVoluenteerInfo } from "../api";
-async function volunteerForm() {
-  async function sendData(formData) {
-    "use server";
+function volunteerForm() {
+  function sendData(formData) {
     console.log("sending data");
 
     const data = {
@@ -9,30 +8,54 @@ async function volunteerForm() {
       last_name: formData.get("last_name"),
       email: formData.get("email"),
     };
-    await postVoluenteerInfo(data);
+    postVoluenteerInfo(data);
   }
 
   return (
     <section className="bg-primary p-8 -mx-mobile text-main-1 max-w-2xl sm:w-3/4 lg:-mx-desktop sm:self-center rounded-rounded-reg">
       <form action={sendData} className="flex flex-col gap-8 ">
         <fieldset>
-          <legend className="font-rethink text-2xl mb-6">Contact information</legend>
+          <legend className="font-rethink text-2xl mb-6">
+            Contact information
+          </legend>
           <div className="grid grid-cols-1 gap-4 align-baseline justify-end ">
             <div className="flex flex-col gap-1">
               <label>First Name</label>
-              <input type="text" name="first_name" placeholder="John" required></input>
+              <input
+                type="text"
+                name="first_name"
+                placeholder="John"
+                required
+              ></input>
             </div>
             <div className="flex flex-col">
               <label>Last Name</label>
-              <input type="text" name="last_name" placeholder="Doe" required></input>
+              <input
+                type="text"
+                name="last_name"
+                placeholder="Doe"
+                required
+              ></input>
             </div>
             <div className="flex flex-col">
               <label>Email</label>
-              <input input="email" type="email" name="email" placeholder="e.g. john@doe.com" required></input>
+              <input
+                input="email"
+                type="email"
+                name="email"
+                placeholder="e.g. john@doe.com"
+                required
+              ></input>
             </div>
             <div className="flex flex-col">
               <label>Phone Number</label>
-              <input type="tel" inputMode="decimal" placeholder="XXXX3095" maxLength="8" required></input>
+              <input
+                type="tel"
+                inputMode="decimal"
+                placeholder="XXXX3095"
+                maxLength="8"
+                required
+              ></input>
             </div>
           </div>
         </fieldset>
--- src/app/volunteer/page.js
+++ src/app/volunteer/page.js
@@ -1,16 +1,32 @@
-import { motion } from "motion/react";
+"use client";
 import Benefits from "../components/Benefits";
 import ClickableCard1 from "../components/ClickableCard1";
 import VolunteerForm from "../components/VoluenteerForm";
+import { useRef } from "react";
 
 function voluenteer() {
+  const formRef = useRef(null);
+  const scrollToElement = () => {
+    if (formRef.current) {
+      formRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  // Bruger useRef-hook til at pege på volunterform og funktionen scrollToElement trigger en scroll-animation til det element formRef peger på (voluenteer form).
   return (
     <article className="flex flex-col gap-32 ">
       <section className="flex flex-col">
         <h1>BECOME A VOLUENTEER</h1>
         <h2>Do you want to become a part of foofest team 2025?</h2>
-        <p>Join the Team Behind "CampEase" in Camping Area Helheim and Bring Comfort to the Festival life.</p>
-        <button className="button grid place-self-start my-6">Apply now</button>
+        <p>
+          Join the Team Behind "CampEase" in Camping Area Helheim and Bring
+          Comfort to the Festival life.
+        </p>
+        <button
+          onClick={scrollToElement}
+          className="button grid place-self-start my-6"
+        >
+          Apply now
+        </button>
       </section>
       <section>
         <Benefits />
@@ -18,13 +34,23 @@ function voluenteer() {
       <div className="flex flex-col gap-6">
         <h2>Learn more about the jobs</h2>
         <div className="flex flex-wrap items-center justify-center md:justify-between xl:justify-between  gap-10 ">
-          <ClickableCard1 src="/img/tshirt.webp" header="Safe space" alt="img" />
+          <ClickableCard1
+            src="/img/tshirt.webp"
+            header="Safe space"
+            alt="img"
+          />
           <ClickableCard1 src="/img/tshirt.webp" header="Safe hhh" alt="img" />
-          <ClickableCard1 src="/img/tshirt.webp" header="Safe space" alt="img" />
+          <ClickableCard1
+            src="/img/tshirt.webp"
+            header="Safe space"
+            alt="img"
+          />
         </div>
       </div>
       <div className="flex flex-col gap-8 lg:items-center">
-        <h2 className="text-center">Send application today!</h2>
+        <h2 className="text-center" ref={formRef}>
+          Send application today!
+        </h2>
         <VolunteerForm />
       </div>
     </article>
